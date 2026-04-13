7bf032e265aa993f9c0eb87bf11b92e6b596dbb7
4beDesign <abe@4be.design>
2026-04-13 22:46:43 +0900

    Add files via upload

---
+++ keyboards/handwired/dactyl_manuform/portable/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 Shem Sedrick (@ssedrick)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define USB_SUSPEND_WAKEUP_DELAY 500
+#define USB_DEVICE_WAIT_TIMEOUT 2000
+#define WDT_ENABLE
