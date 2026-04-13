056ee5ea212ac37adc48dc6513ff6df874986d37
4beDesign <abe@4be.design>
2026-04-13 22:38:00 +0900

    Add files via upload

---
+++ keyboards/handwired/dactyl_manuform/5x6/config.h
@@ -0,0 +1,10 @@
+// Copyright 2022 Shem Sedrick (@ssedrick)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define EE_HANDS
+#define SPLIT_USB_DETECT
+#define USB_SUSPEND_WAKEUP_DELAY 500
+#define USB_DEVICE_WAIT_TIMEOUT 2000
+#define WDT_ENABLE
